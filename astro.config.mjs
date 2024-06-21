6f0e500074f9a487366366bb4e8a5fb9c974b7f3
zachczx <zixian.chen@gmail.com>
2024-06-22 00:56:15 +0800

    Added astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import mdx from '@astrojs/mdx'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx()],
+    integrations: [tailwind(), mdx(), sitemap()],
     prefetch: true,
+    site: 'https://btonomics.com',
 })
