7be6111fc9df855b6884dc91175c09f01ab6300c
zachczx <zixian.chen@gmail.com>
2024-06-25 01:00:00 +0800

    Added pagefile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import pagefind from 'astro-pagefind'
 
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx(), sitemap()],
+    build: {
+        format: 'file',
+    },
+    integrations: [tailwind(), mdx(), pagefind(), sitemap()],
     prefetch: true,
     site: 'https://btonomics.com',
 })
