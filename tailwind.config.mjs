d6a25e475cc6d284b13fa835146cf9c97cc7b879
zachczx <zixian.chen@gmail.com>
2024-07-20 00:57:24 +0800

    Bump dep

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {},
+        fontFamily: {
+            sans: ['Nunito Sans'],
+            serif: ['Roboto Slab Variable'],
+        },
     },
     daisyui: {
         themes: [
