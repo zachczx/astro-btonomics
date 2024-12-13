1cd5dcdea76419aa47f8f517b5f911e35dcdac13
zachczx <zixian.chen@gmail.com>
2024-12-13 23:38:32 +0800

    Fixed issue with Pagefind indexing random tag/category pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, passthroughImageService } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import pagefind from 'astro-pagefind'
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
     build: {
-        format: 'directory',
+        format: 'file', // 'file' is needed for Pagefind to work
     },
     site: 'https://btonomics.com',
     integrations: [tailwind(), mdx(), pagefind(), sitemap()],
